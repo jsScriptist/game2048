8d8ca80d79cf33b34aa7e76bce0a147988bafe64
Andrii Khomenko <khomenkoam007@gmail.com>
2020-09-28 11:49:37 +0300

    Deleted comments.

--- src/app/modules/game/components/game/game.component.ts
+++ src/app/modules/game/components/game/game.component.ts
@@ -22,20 +22,6 @@ const colorMap: {[k: number]: string} = {
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  // colorMap: {[k: number]: string} = {
-  //   2: '#FF5733',
-  //   4: '#FF8933',
-  //   8: '#c4961a',
-  //   16: '#aaa006',
-  //   32: '#84990c',
-  //   64: '#3d960d',
-  //   128: '#0b8854',
-  //   256: '#089398',
-  //   512: '#3385FF',
-  //   1024: '#3338FF',
-  //   2048: '#8A33FF'
-  // };
   keyEventCodMap: {[type: string]: string} = {
     ArrowUp: 'up',
     ArrowDown: 'down',
@@ -43,12 +29,10 @@ export class GameComponent implements OnInit {
     ArrowRight: 'right'
   };
   constructor(public gameService: GameService) { }
-
   ngOnInit(): void {
   }
   getStyles(item: Item): {[p: string]: string} {
     const top = (item.row * 110 - 100) + 'px';
-    // const left = (10 + (item.col - 1) * 100) + 'px';
     const left = (item.col * 110 - 100) + 'px';
     return {top, left, 'background-color': colorMap[item.value] || 'black'};
   }
@@ -59,5 +43,4 @@ export class GameComponent implements OnInit {
       console.log(this.keyEventCodMap[event.code]);
     }
   }
-
 }
--- src/app/modules/game/services/game.service.ts
+++ src/app/modules/game/services/game.service.ts
@@ -5,63 +5,6 @@ import {Item} from '../models/item';
   providedIn: 'root'
 })
 export class GameService {
-  // items: Item[] = [
-  //   {
-  //     value: 2,
-  //     row: 1,
-  //     col: 1
-  //   },
-  //   {
-  //     value: 4,
-  //     row: 1,
-  //     col: 2
-  //   },
-  //   {
-  //     value: 8,
-  //     row: 1,
-  //     col: 3
-  //   },
-  //   {
-  //     value: 16,
-  //     row: 1,
-  //     col: 4
-  //   },
-  //   {
-  //     value: 32,
-  //     row: 2,
-  //     col: 1
-  //   },
-  //   {
-  //     value: 64,
-  //     row: 2,
-  //     col: 2
-  //   },
-  //   {
-  //     value: 128,
-  //     row: 2,
-  //     col: 3
-  //   },
-  //   {
-  //     value: 256,
-  //     row: 2,
-  //     col: 4
-  //   },
-  //   {
-  //     value: 512,
-  //     row: 3,
-  //     col: 1
-  //   },
-  //   {
-  //     value: 1024,
-  //     row: 3,
-  //     col: 2
-  //   },
-  //   {
-  //     value: 2048,
-  //     row: 3,
-  //     col: 3
-  //   },
-  // ];
   private size = 4;
   private availableCells: number[] = [];
   private  get emptyCells(): number[] {
@@ -122,7 +65,6 @@ export class GameService {
       let merged = false;
       let prevItem: Item = null;
       for (const item of items) {
-        // const item: Item = rowItems[i];
         if (prevItem) {
           if (merged) {
             merged = false;
